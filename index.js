53d4cdd69c4b533930601943024561cf0c651054
Carl Ngovi <carl.ngovi@student.moringaschool.com>
2024-03-21 05:06:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 function scuberGreetingForFeet(feet){
-  // Write your code here!
-  if (feet <= 400){
-    return 'This one is on me!';
-  }
-  else if (feet < 2000 && feet > 400){
-    return 'That will be twenty bucks.';
-  }
-  else if (feet > 2000 && feet < 2500){
-    return 'I will gladly take your thirty bucks.';
-  }
-  else if (feet > 2500){
-    return 'No can do.';
-  }
-}
 
-function ternaryCheckCity(city){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  if (feet > 400 && feet < 2000){
+    return "That will be twenty bucks.";
+  }
+  if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (feet > 2500) {
+    return "No can do.";
+  }}
+
+
+function ternaryCheckCity(city){ 
+    return city === 'NYC' ? "Ok, sounds good." :"No go.";
+    }  
   // Write your code here!
-  return city === 'NYC' ? "Ok, sounds good." :"No go.";
-}
+
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
-  {
-    switch (tip) {
-      case " generous" :
-        return "Thank you so much.";
-      case "not as generous" :
-        return "Thank you.";
-
-      default :
-        return "Bye.";
-    }
+  {switch (tip) {
+    case "generous" :
+       return "Thank you so much.";
+    case "not as generous" :
+      return "Thank you.";
+  
+    default:
+      return "Bye." ;
   }
-}
+}}
